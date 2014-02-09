2f87f86f47559572296d1c88ceaf1d8e69e58218
Karen Etheridge <ether@cpan.org>
2014-02-08 23:39:05 -0800

    ensure namespaces are set up properly
    Disabling clean namespaces test when needed; this test passing is conditional
    on the version of MooseX::Types that installs declared types as subs in the
    declaring class, but not in the importing class, so namespace::autoclean works
    properly.

--- lib/MooseX/Types/HTTPMethod.pm
+++ lib/MooseX/Types/HTTPMethod.pm
@@ -5,7 +5,7 @@ package MooseX::Types::HTTPMethod;
 
 use MooseX::Types -declare => [ qw(HTTPMethod10 HTTPMethod11 HTTPMethod) ];
 use MooseX::Types::Moose 'Str';
-use namespace::autoclean;
+use if MooseX::Types->VERSION >= 0.42, 'namespace::autoclean';
 
 my @http10_methods = qw(GET POST HEAD);                     # RFC 1945
 my @http11_methods = (
